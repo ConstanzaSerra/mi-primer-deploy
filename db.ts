01f6365bd7393d18691fbfca7c5bd7e9365fd5d1
Constanza Serra <aserra@frba.utn.edu.ar>
2026-03-30 23:31:19 -0300

    fix firebase credentials for heroku

--- db.ts
+++ db.ts
@@ -4,20 +4,14 @@ import admin from "firebase-admin";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log(
+  "ENV length: ",
+  process.env.FIREBASE_APPLICATION_CREDENTIALS_JSON?.length,
+);
 // Configuración de Firebase usando las variables de entorno
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // Manejar saltos de línea
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-};
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_APPLICATION_CREDENTIALS_JSON!,
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
